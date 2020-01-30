de653a1787ab74da6db511af744dc0d1c3961861
yitznewton <yitznewton@hotmail.com>
2020-01-29 22:55:02 -0500

    add eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const Hi = () => {
-  return <p>Hi.</p>;
+  return <p>Hi?</p>;
 };
 
 render(<Hi />, document.getElementById('app'));
