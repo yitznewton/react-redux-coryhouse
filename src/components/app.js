5f86482bd0b401f58129d58e193297a156455988
yitznewton <yitznewton@hotmail.com>
2020-02-02 22:49:05 -0500

    404

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import AboutPage from "./about/AboutPage";
-import HomePage from "./home/HomePage";
+import { Route, Switch } from 'react-router-dom';
+import Header from './common/header';
+import AboutPage from './about/AboutPage';
+import HomePage from './home/HomePage';
+import PageNotFound from "./PageNotFound";
 
 const App = () => (
   <div className="container-fluid">
-    <Route exact path="/" component={HomePage} />
-    <Route path="/about" component={AboutPage} />
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route component={PageNotFound} />
+    </Switch>
   </div>
 );
 
