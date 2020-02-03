fee0e2b27a5d1ad75433cbf0f06ec01f49bba923
yitznewton <yitznewton@hotmail.com>
2020-02-02 22:52:54 -0500

    Courses page skeleton

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './common/header';
 import AboutPage from './about/AboutPage';
 import HomePage from './home/HomePage';
-import PageNotFound from "./PageNotFound";
+import CoursesPage from './courses/CoursesPage';
+import PageNotFound from './PageNotFound';
 
 const App = () => (
   <div className="container-fluid">
@@ -11,6 +12,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
+      <Route path="/courses" component={CoursesPage} />
       <Route component={PageNotFound} />
     </Switch>
   </div>
