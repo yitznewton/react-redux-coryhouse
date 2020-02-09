abd0c708a6d9a50e6546f5fe64876a9a7d85c40e
yitznewton <yitznewton@hotmail.com>
2020-02-09 12:55:36 -0500

    extract initial state

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import {LOAD_AUTHORS_SUCCESS} from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
     case LOAD_AUTHORS_SUCCESS:
       return action.authors;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case LOAD_COURSES_SUCCESS:
       return action.courses;
